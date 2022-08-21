c6bf0c02f98d7702a27882c7d1006d0afa715371
dastasoft <dastasoft@protonmail.com>
2022-08-21 23:41:40 +0200

    score hud working

---
+++ Main.gd
@@ -0,0 +1,36 @@
+extends Node
+
+export (PackedScene) var mob_scene
+var score = 0
+
+func _ready():
+	randomize()
+
+func new_game():
+	score = 0
+	$ScoreTimer.start()
+	
+
+func game_over():
+	$ScoreTimer.stop()
+
+func _on_MobTimer_timeout():
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.unit_offset = randf()
+	
+	var mob = mob_scene.instance()
+	add_child(mob)
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
