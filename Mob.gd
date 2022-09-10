0411c17da8463e18b1b2926638bedf2f0bb6f339
dastasoft <dastasoft@protonmail.com>
2022-09-10 17:04:37 +0200

    first changes to covid game

--- Mob.gd
+++ Mob.gd
@@ -4,9 +4,10 @@ export var min_speed = 150.0
 export var max_speed = 250.0
 
 func _ready():
-	$AnimatedSprite.playing = true
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	#$AnimatedSprite.playing = true
+	#var mob_types = $AnimatedSprite.frames.get_animation_names()
+	#$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	pass
 
 
 func _on_VisibilityNotifier2D_screen_exited():
